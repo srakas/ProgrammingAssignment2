0cbdbfbacceb68036b1c9fac85c7c8ae3114b8ec
Sonja Rakas <s.rakas@youngculture.com>
2014-12-18 11:48:12 +0100

    Peer assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of the matrix
 
-## Write a short comment describing this function
-
+## Creates matrix cache from the matrix 'x'
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMat <- NULL
+    set <- function(y) {
+        x <<- y
+        inverseMat <<- NULL
+    }
+    
+    get <- function() x
+    setInverseMat <- function() {
+        inverseMat <<- solve(x)    
+    }
+    getInverseMat <- function() inverseMat
+    list(set = set, get = get, setInverseMat = setInverseMat, getInverseMat = getInverseMat)
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## If the matrix did not change and inverse has already been calculated 
+## the inverse is pulled from the cache, otherwise calculates matrix inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseMat <- x$getInverseMat()
+    if (!is.null(inverseMat)) {
+        message("Getting cached data")
+        return(inverseMat)
+    }
+    inverseMat <- x$setInverseMat()
+    inverseMat    
 }
